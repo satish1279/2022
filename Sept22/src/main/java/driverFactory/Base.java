00d94240ebac37e52dc0d8b027a104ec934e1575
SatishJaiswal <SatishJaiswal@DESKTOP-K01PIVP>
2022-10-05 22:14:15 +0530

    new code added

--- Sept22/src/main/java/driverFactory/Base.java
+++ Sept22/src/main/java/driverFactory/Base.java
@@ -10,10 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import util.SStaken;
