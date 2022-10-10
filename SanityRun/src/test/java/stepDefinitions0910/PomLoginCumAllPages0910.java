23a0cc33b1df929ec0200e91e0190168bbc84687
SatishJaiswal <SatishJaiswal@DESKTOP-K01PIVP>
2022-10-10 22:33:32 +0530

    logger added successfully

--- SanityRun/src/test/java/stepDefinitions0910/PomLoginCumAllPages0910.java
+++ SanityRun/src/test/java/stepDefinitions0910/PomLoginCumAllPages0910.java
@@ -3,6 +3,7 @@ package stepDefinitions0910;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -18,18 +19,23 @@ import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.PagesElements0910;
 
+
 public class PomLoginCumAllPages0910 {
 
 
 
-
+	private static final Logger logger= LogManager.getLogger("PomLoginCumAllPages0910");
 	static WebDriver driver =null;
 
+	
+
+
+	
 	PagesElements0910 pages;
 	Screenshots0910 takeSS;
-
-
-
+	
+	
+	
 
 	@Before
 
@@ -43,6 +49,8 @@ public class PomLoginCumAllPages0910 {
 		driver.manage().window().maximize();
 
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		logger.info("Browser Started");
 	}
 
 
@@ -53,6 +61,7 @@ public class PomLoginCumAllPages0910 {
 
 		Thread.sleep(1000);
 		driver.close();
+		logger.info("Browser Closed");
 	}
 
 
@@ -74,6 +83,7 @@ public class PomLoginCumAllPages0910 {
 
 		takeSS = new Screenshots0910(driver);
 		takeSS.takeScreenshot();
+		logger.info("Screenshot of taken for the page - " + driver.getTitle());
 
 	}
 
@@ -85,6 +95,7 @@ public class PomLoginCumAllPages0910 {
 	public void loginPage() throws IOException {
 
 		driver.navigate().to("https://goinfinity.beyond360apps.com/");
+		logger.info("This is login page");
 	}
 
 
@@ -95,7 +106,7 @@ public class PomLoginCumAllPages0910 {
 
 		pages.entrUsernam();
 		pages.entrPassword();
-		pages.clikButon();
+		//pages.clikButon();
 	}
 
 
@@ -104,6 +115,7 @@ public class PomLoginCumAllPages0910 {
 	public void recogLink() throws InterruptedException {
 		//System.out.println("test");
 		pages.clickRecognize();
+		logger.info("This is Recognize page");
 
 	}
 
@@ -113,6 +125,7 @@ public class PomLoginCumAllPages0910 {
 	public void skillinsightLink() throws InterruptedException {
 		//System.out.println("test");
 		pages.clickSkillInsight();
+		logger.info("This is Feedback page");
 	}
 
 
@@ -121,6 +134,7 @@ public class PomLoginCumAllPages0910 {
 	public void WellnessLink() throws InterruptedException {
 		//System.out.println("test");
 		pages.clickWellness();
+		logger.info("This is Wellness page");
 	}
 
 
@@ -130,6 +144,7 @@ public class PomLoginCumAllPages0910 {
 	public void SurveyLink() throws InterruptedException {
 		//System.out.println("test");
 		pages.clickSurvey();
+		logger.info("This is Survey page");
 	}
 
 
@@ -139,6 +154,7 @@ public class PomLoginCumAllPages0910 {
 	public void MySummaryLink() throws InterruptedException {
 		//System.out.println("test");
 		pages.clickMySummary();
+		logger.info("This is My Summary page");
 
 	}
 
@@ -147,6 +163,7 @@ public class PomLoginCumAllPages0910 {
 	public void RedeemLink() throws InterruptedException {
 		System.out.println("test");
 		//pages.clickRedeem();
+		logger.info("This is Redeem page");
 	}
 
 
@@ -155,6 +172,7 @@ public class PomLoginCumAllPages0910 {
 	public void DashboardLink() throws InterruptedException {
 		System.out.println("test");
 		pages.clickDashboard();	
+		logger.info("This is Dashboard page");
 	}
 
 
@@ -162,6 +180,7 @@ public class PomLoginCumAllPages0910 {
 	public void ReportTab() throws InterruptedException {
 		//System.out.println("test");
 		pages.clickReports();
+		logger.info("This is Reports page");
 	}
 
 	@And ("^Clicks on Budget Head Report option from Select Report dropdown$")
@@ -171,7 +190,8 @@ public class PomLoginCumAllPages0910 {
 		pages.clickSelectReportDropdown();
 		Thread.sleep(1000);
 		pages.clickBudgetHeadReport();
-		
+		logger.info("This is Budget Head Report page");
+
 	}
 
 
@@ -181,7 +201,8 @@ public class PomLoginCumAllPages0910 {
 		pages.clickSelectReportDropdown();
 		Thread.sleep(1000);
 		pages.clickRecognitionsReport();
-		
+		logger.info("This is Recognitions Report page");
+
 	}
 
 	@And ("^Clicks on Redemption Report option from Select Report dropdown$")
@@ -190,7 +211,8 @@ public class PomLoginCumAllPages0910 {
 		pages.clickSelectReportDropdown();
 		Thread.sleep(1000);
 		pages.clickRedemptionReport();
-		
+		logger.info("This is Redemption Report page");
+
 	}
 
 	@When ("^Clicks on User Login Report option from Select Report dropdown$")
@@ -199,7 +221,8 @@ public class PomLoginCumAllPages0910 {
 		pages.clickSelectReportDropdown();
 		Thread.sleep(1000);
 		pages.clickUserLoginReport();
-		
+		logger.info("This is User Login Report page");
+
 	}
 
 
@@ -209,7 +232,8 @@ public class PomLoginCumAllPages0910 {
 		//System.out.println("test");
 
 		pages.clickVisualization();
-		
+		logger.info("This is Data Visualization page");
+
 	}
 
 
@@ -225,6 +249,8 @@ public class PomLoginCumAllPages0910 {
 		Thread.sleep(2000);
 
 		pages.clickOkBtn();
+		
+		logger.info("This is LogOut page");
 	}
 
 
